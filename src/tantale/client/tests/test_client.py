bc6b9f8ce9fd082f6cb728df8e47fddbf34916b6
redref <ref.redcoder@gmail.com>
2016-04-20 22:52:17 +0200

    Client : tests behavior

--- src/tantale/client/tests/test_client.py
+++ src/tantale/client/tests/test_client.py
@@ -14,24 +14,24 @@ my_folder = os.path.dirname(os.path.abspath(__file__))
 diamond_fifo = os.path.join(my_folder, '.test_diamond_fifo')
 nagios_fifo = os.path.join(my_folder, '.test_nagios_fifo')
 
-diamond_input = """servers.my_fqdn.diskspace.root.byte_percentfree 90.90
-servers.my_fqdn.diskspace.root.byte_used 11017080832.00
-servers.my_fqdn.diskspace.root.byte_free 109989548032.00
-servers.my_fqdn.diskspace.root.byte_avail 103819173888.00
-servers.my_fqdn.diskspace.root.inodes_percentfree 95
-servers.my_fqdn.diskspace.root.inodes_used 340616
-servers.my_fqdn.diskspace.root.inodes_free 7171448
-servers.my_fqdn.diskspace.root.inodes_avail 7171448
-servers.my_fqdn.cpu.total.system 1
-servers.my_fqdn.cpu.total.user 3
-servers.my_fqdn.cpu.total.softirq 0
-servers.my_fqdn.cpu.total.nice 0
-servers.my_fqdn.cpu.total.steal 0
-servers.my_fqdn.cpu.total.iowait 0
-servers.my_fqdn.cpu.total.guest 0
-servers.my_fqdn.cpu.total.guest_nice 0
-servers.my_fqdn.cpu.total.idle 396
-servers.my_fqdn.cpu.total.irq 0"""
+diamond_input = """servers.domain.my_fqdn.diskspace.root.byte_percentfree 90.90
+servers.domain.my_fqdn.diskspace.root.byte_used 11017080832.00
+servers.domain.my_fqdn.diskspace.root.byte_free 109989548032.00
+servers.domain.my_fqdn.diskspace.root.byte_avail 103819173888.00
+servers.domain.my_fqdn.diskspace.root.inodes_percentfree 95
+servers.domain.my_fqdn.diskspace.root.inodes_used 340616
+servers.domain.my_fqdn.diskspace.root.inodes_free 7171448
+servers.domain.my_fqdn.diskspace.root.inodes_avail 7171448
+servers.domain.my_fqdn.cpu.total.system 1
+servers.domain.my_fqdn.cpu.total.user 3
+servers.domain.my_fqdn.cpu.total.softirq 0
+servers.domain.my_fqdn.cpu.total.nice 0
+servers.domain.my_fqdn.cpu.total.steal 0
+servers.domain.my_fqdn.cpu.total.iowait 0
+servers.domain.my_fqdn.cpu.total.guest 0
+servers.domain.my_fqdn.cpu.total.guest_nice 0
+servers.domain.my_fqdn.cpu.total.idle 396
+servers.domain.my_fqdn.cpu.total.irq 0"""
 
 
 class ClientTC(TantaleTC):
@@ -80,7 +80,8 @@ class ClientTC(TantaleTC):
         for nb in range(10):
             time.sleep(1)
             live_s.send(
-                self.getLivestatusRequest('get_service') % ("fqdn", "root"))
+                self.getLivestatusRequest('get_service') %
+                ("fqdn.domain", "root"))
             res = live_s.recv()
             res = eval(res[16:])
             if len(res) > 0:
@@ -99,21 +100,43 @@ class ClientTC(TantaleTC):
             fd,
             bytes(
                 "[%d] PROCESS_SERVICE_CHECK_RESULT;%s;%s;%d;%s\n" %
-                (int(time.time()), "fqdn", "Host", 0, "test_output")))
+                (int(time.time()), "fqdn.domain", "Host", 0, "test_output")))
+        # Empty output + users
+        os.write(
+            fd,
+            bytes(
+                "[%d] PROCESS_SERVICE_CHECK_RESULT;%s;%s;%d;%s\n" %
+                (int(time.time()), "fqdn2.domain", "Host", 0, "")))
         os.close(fd)
 
         # Check result from livestatus
         live_s = self.getSocket('Livestatus')
         for nb in range(10):
             time.sleep(1)
-            live_s.send(self.getLivestatusRequest('get_host') % ("fqdn"))
+            live_s.send(
+                self.getLivestatusRequest('get_host') % ("fqdn.domain"))
             res = live_s.recv()
             res = eval(res[16:])
             if len(res) > 0:
                 break
 
-        self.assertEqual(res[0][-3], "fqdn")
+        self.assertEqual(res[0][-3], "fqdn.domain")
         self.assertEqual(res[0][4], "test_output")
+        self.assertEqual(res[0][13], 0)
+
+        # Check result from livestatus
+        live_s = self.getSocket('Livestatus')
+        for nb in range(10):
+            time.sleep(1)
+            live_s.send(
+                self.getLivestatusRequest('get_host') % ("fqdn2.domain"))
+            res = live_s.recv()
+            res = eval(res[16:])
+            if len(res) > 0:
+                break
+
+        self.assertEqual(res[0][-3], "fqdn2.domain")
+        self.assertEqual(res[0][4], "")
         self.assertEqual(res[0][13], 0)
 
         self.stop()
